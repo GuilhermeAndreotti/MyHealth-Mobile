5594496426973b476c005a6343e17cf29b335e65
GuilhermeAndreotti <gandreotti@alunos.utfpr.edu.br>
2023-05-03 21:39:14 -0300

    tudo

---
+++ MyHealth/index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import {name as appName} from './app.json';
+import Navi from './src/screens/Navigation';
+
+AppRegistry.registerComponent(appName, () => Navi);
